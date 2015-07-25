2a04623d83a562afc75887522a7fa2b641c2772b
Pradhyo Bijja <pradhyo@nyu.edu>
2015-07-25 14:33:42 -0400

    Code to load excel sheet into database

--- DjangoWebProject1/app/models.py
+++ DjangoWebProject1/app/models.py
@@ -1,9 +1,31 @@
 from django.db import models
+from pyzipcode import ZipCodeDatabase
+import pandas as pd
+import random
+
+def load_data(fileName='table.xls'):
+    for row in pd.read_excel(fileName).iterrows():
+        customer = MasterTable(
+                    cl=row[1]['CL_ID'],
+                    esda=row[1]['ESDA_ALTERNATE_PRODUCT_CD'],
+                    contract=row[1]['CMR_CONTRACT_ID'],
+                    first_name=row[1]['FIRST_NAME'],
+                    last_name=row[1]['LAST_NAME'],
+                    street_address=row[1]['STR_ONE_AD'],
+                    city=row[1]['CTY_AD'],
+                    state=row[1]['STATE_CD'],
+                    zipcode=int(row[1]['ZIP']),
+                    extend_zip=int(row[1]['EXTND_ZIP_CD']),
+                    country=row[1]['CTY_AD'],
+                    us_county=int(row[1]['US_CNTY_CD']),
+                    is_male=True if row[1]['GENDER_CD'] else False,
+                    )
+        customer.save()
 
 class MasterTable(models.Model):
-    cl = models.IntegerField(blank=True)
+    cl = models.IntegerField(blank=True, null=True)
     esda = models.CharField(blank=True, max_length=20)
-    contract = models.IntegerField(blank=True)
+    contract = models.CharField(blank=True, max_length=20)
     first_name = models.CharField(blank=True, max_length=50)
     last_name = models.CharField(blank=True, max_length=50)
     street_address = models.CharField(blank=True, max_length=100)
@@ -14,3 +36,6 @@ class MasterTable(models.Model):
     country = models.CharField(max_length=3, default='U')
     us_county = models.IntegerField(blank=True)
     is_male = models.BooleanField(blank=True)
+
+    def __unicode__(self):
+        return self.first_name + ' ' + self.last_name
