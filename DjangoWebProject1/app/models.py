220fbb1240bde857356b2c3d51d6c20f5205eb0e
Pradhyo Bijja <pradhyo@nyu.edu>
2015-07-25 12:53:24 -0400

    Remove heatmap project

--- DjangoWebProject1/app/models.py
+++ DjangoWebProject1/app/models.py
@@ -1,5 +1,16 @@
-"""
-Definition of models.
-"""
-
 from django.db import models
+
+class MasterTable(models.Model):
+    cl = models.IntegerField(blank=True)
+    esda = models.CharField(blank=True)
+    contract = models.IntegerField(blank=True)
+    first_name = models.CharField(blank=True)
+    last_name = models.CharField(blank=True)
+    street_address = models.CharField(blank=True)
+    city = models.CharField(blank=True)
+    state =models.CharField(blank=True, max_length=2)
+    zipcode = models.IntegerField(blank=True)
+    extend_zip = models.IntegerField(blank=True)
+    country = models.CharField(max_length=3, default='U')
+    us_county = models.IntegerField(blank=True)
+    is_male = BooleanField(blank=True, null=True)
--- heatmap/map/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def master_table(request):
-    return render(request, 'map/index.html')
